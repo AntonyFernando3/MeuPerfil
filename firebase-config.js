5f48edae2ddf80351469815a252a9583f0ab121c
Antony Fernando <89105412+AntonyFernando3@users.noreply.github.com>
2026-02-08 23:05:27 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,17 +2,15 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
-// Suas chaves do Console do Firebase
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
+  apiKey: "AIzaSyDB3xuL_s5GslT60J3YKHCwFipo7pDl6L8",
   authDomain: "antonytechnology.firebaseapp.com",
   projectId: "antonytechnology",
   storageBucket: "antonytechnology.appspot.com",
-  messagingSenderId: "SEU_ID",
-  appId: "SEU_APP_ID"
+  messagingSenderId: "1005968563727",
+  appId: "1:1005968563727:web:f0a2cf91b463b0f9684d7a"
 };
 
-// Inicialização
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app); // Exporta para usar no login/cadastro
-export const db = getFirestore(app); // Exporta para usar no banco de dados
+export const auth = getAuth(app);
+export const db = getFirestore(app);
